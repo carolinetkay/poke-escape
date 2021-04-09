9352a6de0822af7432e21183db7d87e6f57188da
carolinetan <caroline.kh.tan@gmail.com>
2021-04-09 22:12:54 +0800

    added event listener to buttons

--- map.js
+++ map.js
@@ -12,5 +12,22 @@ function create2DArray(rows, columns, value = (x, y) => 0) {
   }
   
   var array = create2DArray(5, 5, (row, column) => row + column);
-  
-  console.log(array);
+
+//   console.log(array);
+
+function gamepadMovement (e) {
+    console.log(e.key);
+
+    // if (e.key === 'ArrowDown') {
+    //     console.log('down');
+    // }
+
+    switch (e.key) {
+        case 'ArrowDown': console.log('down pressed'); break;
+        case 'ArrowUp': console.log('up pressed'); break;
+        case 'ArrowLeft': console.log('left pressed'); break;
+        case 'ArrowRight': console.log('right pressed'); break;
+    }
+}
+
+document.addEventListener('keydown', gamepadMovement);
