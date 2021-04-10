da40045200f966f0eef7d98cbb89086037281577
carolinetan <caroline.kh.tan@gmail.com>
2021-04-10 17:47:39 +0800

    added coordinates and moving star

--- map.js
+++ map.js
@@ -1,36 +1,58 @@
-'use strict';
+"use strict";
 
 function create2DArray(rows, columns, value = (x, y) => 0) {
-    var array = new Array(rows);
-    for (var i = 0; i < rows; i++) {
-      array[i] = new Array(columns);
-      for (var j = 0; j < columns; j++) {
+  let array = new Array(rows);
+  for (let i = 0; i < rows; i++) {
+    array[i] = new Array(columns);
+    for (let j = 0; j < columns; j++) {
+      if ((i === starCoordinates.x) & (j === starCoordinates.y)) {
+        array[i][j] = "*";
+      } else {
         array[i][j] = value(i, j);
       }
     }
-    return array;
   }
-
-  var array = create2DArray(5, 5, (row, column) => row + column);
-
-  console.log(array);
-
-function gamepadMovement (e) {
-    // console.log(e.key);
-    switch (e.key) {
-        case 'ArrowDown':
-          console.log('down pressed');
-          var elem = document.querySelector('#map-container');
-          elem.innerHTML = array.map(function(a) {
-            return a + '<br>';
-          });
-          break;
-        case 'ArrowUp': console.log('up pressed'); break;
-        case 'ArrowLeft': console.log('left pressed'); break;
-        case 'ArrowRight': console.log('right pressed'); break;
-    }
+  return array;
 }
 
-window.addEventListener('load', function () {
-  document.addEventListener('keydown', gamepadMovement);
+let starCoordinates = { x: 0, y: 0 };
+// let array = create2DArray(5, 5, (row, column) => row + column);
+const displayMap = function () {
+    const array = create2DArray(5, 5, (row, column) => row + column);
+    document.querySelector("#map-container").innerHTML = array.map(function (a) {
+      return a + "<br>"
+    });
+}
+
+
+function gamepadMovement(e) {
+  switch (e.key) {
+    case "ArrowDown":
+      console.log("down pressed");
+      starCoordinates.x = starCoordinates.x + 1;
+      displayMap();
+      break;
+    case "ArrowUp":
+      console.log("up pressed");
+      starCoordinates.x = starCoordinates.x - 1;
+      displayMap();
+      break;
+    case "ArrowLeft":
+      console.log("left pressed");
+      starCoordinates.y = starCoordinates.y - 1;
+      displayMap();
+      break;
+    case "ArrowRight":
+      console.log("right pressed");
+      starCoordinates.y = starCoordinates.y + 1;
+      displayMap();
+      break;
+  }
+}
+
+window.addEventListener("load", function () {
+  document.addEventListener("keydown", gamepadMovement);
 });
+
+// 1. override the top left number with a star
+// 2. add to the eventlistener, when button is pressed then star moves to the next element in the array of the array
