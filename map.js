9e2487c3238aa0885d78648d82481e9db540d7b6
carolinetan <caroline.kh.tan@gmail.com>
2021-04-10 16:11:09 +0800

    listeners

--- map.js
+++ map.js
@@ -13,15 +13,10 @@ function create2DArray(rows, columns, value = (x, y) => 0) {
   
   var array = create2DArray(5, 5, (row, column) => row + column);
 
-//   console.log(array);
+  console.log(array);
 
 function gamepadMovement (e) {
-    console.log(e.key);
-
-    // if (e.key === 'ArrowDown') {
-    //     console.log('down');
-    // }
-
+    // console.log(e.key);
     switch (e.key) {
         case 'ArrowDown': console.log('down pressed'); break;
         case 'ArrowUp': console.log('up pressed'); break;
