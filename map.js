6b582e0e24755146f4b7eba10b4d152876e876a8
Fendy <fendy.fendy95@gmail.com>
2021-04-10 16:36:37 +0800

    Add minimum array display

--- map.js
+++ map.js
@@ -10,7 +10,7 @@ function create2DArray(rows, columns, value = (x, y) => 0) {
     }
     return array;
   }
-  
+
   var array = create2DArray(5, 5, (row, column) => row + column);
 
   console.log(array);
@@ -18,11 +18,19 @@ function create2DArray(rows, columns, value = (x, y) => 0) {
 function gamepadMovement (e) {
     // console.log(e.key);
     switch (e.key) {
-        case 'ArrowDown': console.log('down pressed'); break;
+        case 'ArrowDown':
+          console.log('down pressed');
+          var elem = document.querySelector('#map-container');
+          elem.innerHTML = array.map(function(a) {
+            return a + '<br>';
+          });
+          break;
         case 'ArrowUp': console.log('up pressed'); break;
         case 'ArrowLeft': console.log('left pressed'); break;
         case 'ArrowRight': console.log('right pressed'); break;
     }
 }
 
-document.addEventListener('keydown', gamepadMovement);
+window.addEventListener('load', function () {
+  document.addEventListener('keydown', gamepadMovement);
+});
